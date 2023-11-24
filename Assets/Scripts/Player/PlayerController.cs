8aa8cf2ad1c34c401b0b1d10f8928661d165792b
Birand Kılınç <pacmaidanapac@gmail.com>
2023-11-24 13:34:47 +0300

    Player Movement using joystick is completed

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class PlayerController : MonoBehaviour
+{
+    [Inject] PlayerMovement playerMovement;
+  
+    private void OnEnable()
+    {
+        PlayerInput.OnPlayerInput += HandleAngularMovement;
+        PlayerInput.OnPlayerInput += HandleMovement;
+    }
+    private void HandleMovement(Vector3 movementVector)
+    {
+        playerMovement.CharacterMovement(movementVector);
+    }
+    private void HandleAngularMovement(Vector3 angularMoveVector)
+    {
+        playerMovement.CharacterAngularMovement(angularMoveVector);
+    }
+
+    private void OnDisable()
+    {
+        PlayerInput.OnPlayerInput -= HandleAngularMovement;
+        PlayerInput.OnPlayerInput -= HandleMovement;
+    }
+}
